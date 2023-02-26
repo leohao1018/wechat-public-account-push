3955330547f08b41d371f5519c2e46f0d6ad9107
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 17:00:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,28 +23,28 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olaIU6Rea3fpSyvGWmOpJuDR3VdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'UgEUyeR6g6piCiMSxiAyrKVE9W3crtHFdGd7734ADBE',
+      useTemplateId: 'EX3vrHo3aG5O85faRCXATM5SxuCFXVJAi2d83YRyiao',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-18',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '04-02',
+          type: '*农历生日', name: '宝贝', year: '1996', date: '04-02',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        //{
-        //  type: '生日', name: '李四', year: '1996', date: '09-31',
-        //},
-        //{
-        //  type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
-        //},
+        {
+          type: '阳历生日', name: '宝贝', year: '1996', date: '05-18',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-01-26' },
+        { keyword: 'love_day', date: '2023-02-14' },
         // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
