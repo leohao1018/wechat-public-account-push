920a4b6678104d759b1e5c6dfdef31c87bcf9aed
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 17:02:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olaIU6Rea3fpSyvGWmOpJuDR3VdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EX3vrHo3aG5O85faRCXATM5SxuCFXVJAi2d83YRyiao',
+      useTemplateId: 'O0kjlso6IKb09OO7QRgdOFKKukJGP87axhPFHBEgfzU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-18',
       festivals: [
