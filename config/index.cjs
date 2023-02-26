7dc2e2c1306f5fdb00f07c9e4ae89d9e235dd03b
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 17:31:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -35,16 +35,16 @@ const USER_CONFIG = {
         //{
         //  type: '生日', name: '宝贝', year: '1996', date: '05-18',
         //},
-        //{
-        //  type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
-        //},
+        {
+          type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-14' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
     
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'Q2d4VIQsXdt4OnT1kon-eBwCCOD7dRVEmDR4uiAhAm8',
   CALLBACK_USERS: [
     {
       name: '自己',
