4187e2136b0e644cee2e0faf7ef4888c8b9a5bf8
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 17:20:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olaIU6Rea3fpSyvGWmOpJuDR3VdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EX3vrHo3aG5O85faRCXATM5SxuCFXVJAi2d83YRyiao',
+      useTemplateId: 'HtnUSrvFxSIZftVJ3S4wbO8rV7ZTn91OvWDc0-KOZNs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-18',
       festivals: [
@@ -48,42 +48,12 @@ const USER_CONFIG = {
       ],
     },
     
-    {
-      // 想要发送的人的名字
-      name: '宝贝',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'olaIU6c-_z321NYZ_i3OPO4LbwwI',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'EX3vrHo3aG5O85faRCXATM5SxuCFXVJAi2d83YRyiao',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '05-18',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*农历生日', name: '宝贝', year: '1996', date: '04-02',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '阳历生日', name: '宝贝', year: '1996', date: '05-18',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-02-14' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
-    },
+    
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
-
+  CALLBACK_TEMPLATE_ID: '0002',
   CALLBACK_USERS: [
     {
       name: '自己',
