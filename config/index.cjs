03bc8f6e194369a45e12f3e543a225740b788fe2
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 16:51:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -35,14 +35,14 @@ const USER_CONFIG = {
         //{
         //  type: '生日', name: '李四', year: '1996', date: '09-31',
         //},
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
-        },
+        //{
+        //  type: '节日', name: '相识纪念日', year: '2023', date: '01-26',
+        //},
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-02-14' },
+        { keyword: 'love_day', date: '2023-01-26' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
