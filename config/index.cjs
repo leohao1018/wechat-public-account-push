8954b72340ab3dfacd477102bcc1608d4318b9f8
leohao1018 <liang.hao.1018@outlook.com>
2023-02-26 16:52:16 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olaIU6Rea3fpSyvGWmOpJuDR3VdI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3EeIwKPhfJnHkqKMoooVAY_84XWoxjxKGH1_XcEeuAs',
+      useTemplateId: 'UgEUyeR6g6piCiMSxiAyrKVE9W3crtHFdGd7734ADBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-18',
       festivals: [
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-01-26' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
